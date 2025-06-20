b6dfb6df7f98f4268efb04f38e5ffc1a6b03d939
ZiGaXOXO <zekasulmin@gmail.com>
2025-06-21 05:15:59 +1000

    Add HelloAPIView with get and post

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class HelloAPIView(APIView):
+    def get(self, request):
+        data = {'message': 'Hello, world!'}
+        return Response(data)
+
+    def post(self, request):
+        name = request.data.get('name')
+        if not name:
+            return Response({'error': 'Name is required'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': f'Hello, {name}!'})
