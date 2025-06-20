71fde546f32cad84d16c86b36abe479ae7f65611
ZiGaXOXO <zekasulmin@gmail.com>
2025-06-21 03:48:33 +1000

    Add Todo model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='todos')
+
+    def __str__(self):
+        return self.title
