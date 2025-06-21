018fb1939183089b8aa8b3662e490ac77c7e9d34
ZiGaXOXO <zekasulmin@gmail.com>
2025-06-22 06:32:03 +1000

    Add login_view and logout_view

--- lab2/views.py
+++ lab2/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 def register_view(request):
     if request.method == 'POST':
@@ -12,3 +14,21 @@ def register_view(request):
     else:
         form = RegistrationForm()
     return render(request, 'lab2/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('lab2:profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'lab2/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('lab2:login')
