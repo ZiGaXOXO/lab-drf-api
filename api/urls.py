67dec4200fbc8240bad31a7462f110d242097313
ZiGaXOXO <zekasulmin@gmail.com>
2025-06-22 03:02:24 +1000

    full CRUD in APIView

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HelloAPIView
+from .views import *
 
 urlpatterns = [
     path('hello/', HelloAPIView.as_view(), name='hello'),
+    path('todos/', TodoListCreateAPIView.as_view(), name='todo-list-create'),
+    path('todos/<int:pk>/', TodoDetailAPIView.as_view(), name='todo-detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .serializers import TodoSerializer
 from .models import Todo
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -33,3 +34,41 @@ class HelloAPIView(APIView):
         if not name:
             return Response({'error': 'Name is required'}, status=status.HTTP_400_BAD_REQUEST)
         return Response({'message': f'Hello, {name}!'})
+
+class TodoDetailAPIView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_object(self, pk):
+        return get_object_or_404(Todo, pk=pk)
+
+    def get(self, request, pk):
+        todo = self.get_object(pk)
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        todo = self.get_object(pk)
+        if todo.owner != request.user:
+            return Response({'detail': 'Не авторизованы для изменения этой задачи'}, status=status.HTTP_403_FORBIDDEN)
+        serializer = TodoSerializer(todo, data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save(owner=request.user)
+        return Response(serializer.data)
+
+    def patch(self, request, pk):
+        todo = self.get_object(pk)
+        if todo.owner != request.user:
+            return Response({'detail': 'Не авторизованы для изменения этой задачи'}, status=status.HTTP_403_FORBIDDEN)
+        serializer = TodoSerializer(todo, data=request.data, partial=True)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save(owner=request.user)
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        todo = self.get_object(pk)
+        if todo.owner != request.user:
+            return Response({'detail': 'Не авторизованы для удаления этой задачи'}, status=status.HTTP_403_FORBIDDEN)
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
