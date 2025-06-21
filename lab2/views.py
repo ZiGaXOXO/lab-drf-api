b5c53f56152c3ed0077bb6e59e5bd97522c2a7ce
ZiGaXOXO <zekasulmin@gmail.com>
2025-06-22 06:24:51 +1000

    Add register_view

--- lab2/views.py
+++ lab2/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import RegistrationForm
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Регистрация успешна. Пожалуйста, войдите.")
+            return redirect('lab2:login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'lab2/register.html', {'form': form})
