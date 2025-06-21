a3f5ba77515d71e3403e98de144d996c62773542
ZiGaXOXO <zekasulmin@gmail.com>
2025-06-22 02:05:34 +1000

    Fix authentication for HelloAPIView

--- api/views.py
+++ api/views.py
@@ -1,8 +1,29 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import TodoSerializer
+from .models import Todo
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import AllowAny
+
+class TodoListCreateAPIView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    def get(self, request):
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class HelloAPIView(APIView):
+    permission_classes = [AllowAny]
     def get(self, request):
         data = {'message': 'Hello, world!'}
         return Response(data)
