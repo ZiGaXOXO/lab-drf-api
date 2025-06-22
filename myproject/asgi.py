60bea5ffa0e7164029f56fb6849af6c202f4f049
ZiGaXOXO <zekasulmin@gmail.com>
2025-06-22 16:36:23 +1000

    Add asgi

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,16 +1,16 @@
-"""
-ASGI config for myproject project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+from products.consumers import ProductCountConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": URLRouter([
+        path('ws/products/count/', ProductCountConsumer.as_asgi()),
+    ]),
+})
